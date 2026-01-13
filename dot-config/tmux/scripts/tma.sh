37574a46cf8dd11922706741b0e68179ed81956e
Riccardo Strina <riccardo.strina@icloud.com>
2026-01-13 23:26:12 +0100

    Add fzf tmux session scripts

---
+++ dot-config/tmux/scripts/tma.sh
@@ -0,0 +1,11 @@
+# fzf search existing tmux sessions or create a new one named with query
+tma() {
+  local query session
+  { read -r query; read -r session } <<< "$(tmux ls -F '#{session_name}' 2>/dev/null | fzf \
+      --style minimal --tmux --reverse --height=60% \
+      --border --algo=v1 --prompt="attach> " \
+      --preview-label=" tmux sessions " --preview-label-pos=top --preview-window=border-rounded \
+    --preview '$HOME/.config/tmux/scripts/tmux_tree_arc.sh {} | bat --style=plain --color=always' \
+    --preview-window=right:50% --print-query)"
+  [[ -n "$session" ]] && tmux attach -t "$session" || [[ -n "$query" ]] && tmux new -s "$query"
+}
---
+++ dot-config/tmux/scripts/tmux_tree_arc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+sessions=$(tmux ls -F'#{session_id}:#{session_name}: #{T:tree_mode_format}')
+while IFS=: read -r sid sname srest; do
+    if [[ "$1" == "$sname" ]]; then
+        echo -e "\033[1;34m $sname: $srest\033[0m"
+    else
+        echo -e "\033[1m $sname: $srest\033[0m"
+    fi
+    windows=$(tmux lsw -t"$sid" -F'#{window_index}:#{window_name}:#{?window_zoomed_flag, ,}')
+    window_count=$(echo "$windows" | wc -l)
+    window_num=0
+    while IFS=: read -r widx wname wzoom; do
+        ((window_num++))
+        if ((window_num == window_count)); then
+            echo "  ╰─  $wname$wzoom [$widx]"
+            prefix="    "
+        else
+            echo "  ├─  $wname$wzoom [$widx]"
+            prefix="  │ "
+        fi
+        panes=$(tmux list-panes -t"$sid:$widx" -F'#{pane_index}:#{pane_current_command}:#{pane_current_path}')
+        pane_count=$(echo "$panes" | wc -l)
+        pane_num=0
+        while IFS=: read -r pidx pcmd ppath; do
+            ((pane_num++))
+            if ((pane_num == pane_count)); then
+                echo "$prefix   ╰─  $pidx: $pcmd [${ppath/#$HOME/~}]"
+            else
+                echo "$prefix   ├─  $pidx: $pcmd [${ppath/#$HOME/~}]"
+            fi
+        done <<< "$panes"
+    done <<< "$windows"
+done <<< "$sessions"
